4eacaf51630441b7f0047707e16322579c16fab3
Joe Dytrych <me@jdytrych.co.uk>
2014-05-01 23:23:39 +0100

    less strict json validation

--- api/utils.go
+++ api/utils.go
@@ -22,10 +22,6 @@ func parseReqJSON(r *http.Request, d interface{}) (error, int) {
 		return fmt.Errorf("You must supply a body"), http.StatusBadRequest
 	}
 
-	if r.Header["Content-Type"][0] != "application/json" {
-		return fmt.Errorf("Content-Type must be application/json"), http.StatusBadRequest
-	}
-
 	dcd := json.NewDecoder(r.Body)
 	if err := dcd.Decode(d); err != nil {
 		return fmt.Errorf("Could not decode JSON: %v", err), http.StatusBadRequest
