7b44310f98649566a013e2d4785916b1f1e515a8
Joe Dytrych <me@jdytrych.co.uk>
2014-05-01 17:52:42 +0100

    Remove app version code;

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"log"
 	"net/http"
-	"os/exec"
 	"runtime"
-	"strings"
 
 	"./api"
 
@@ -15,15 +12,12 @@ import (
 	"github.com/zenazn/goji/web"
 )
 
-var APP_VERSION string
 var staticPath = flag.String("static", "../E.A.K/public",
 	"The location of the static assets to serve")
 
 func main() {
 	flag.Parse()
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	APP_VERSION = getVersion()
-	log.Println("Version:", APP_VERSION)
 
 	conf := loadConfig()
 	if conf.ApiEnabled {
@@ -40,22 +34,4 @@ func main() {
 
 func NotFound(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "404 :(", http.StatusNotFound)
-}
-
-func getVersion() string {
-	v := run("git", "rev-parse", "HEAD")
-	if run("git", "status", "--porcelain") != "" {
-		v += "_DEV"
-	}
-	return v
-}
-
-func run(name string, args ...string) string {
-	cmd := exec.Command(name, args...)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
-	}
-	return strings.Trim(out.String(), " \t\n\r")
 }
